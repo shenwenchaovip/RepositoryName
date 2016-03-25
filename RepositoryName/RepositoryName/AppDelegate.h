e14db386b27dd67fb0501837d98469fdadf28b03
shenwenchaovip <shenwenchaovip@163.com>
2016-03-25 22:25:23 +0800

    提交信息

---
+++ RepositoryName/RepositoryName/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RepositoryName
+//
+//  Created by 沈文超 on 16/3/25.
+//  Copyright © 2016年 沈文超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RepositoryName/RepositoryName/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RepositoryName
+//
+//  Created by 沈文超 on 16/3/25.
+//  Copyright © 2016年 沈文超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
